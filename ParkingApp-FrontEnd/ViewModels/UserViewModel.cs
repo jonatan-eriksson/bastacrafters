0cd4b1cdc039cd721cd5bae03b4749447a191e0b
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-06-09 20:32:08 +0200

    Add user viewmodel

---
+++ ParkingApp-FrontEnd/ViewModels/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingApp_FrontEnd.ViewModels
+{
+    public class UserViewModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public double Phone { get; set; }
+        public double CardNumber { get; set; }
+        public int CardDate { get; set; }
+        public int CCV { get; set; }
+        public string LicensePlate { get; set; }
+        public string CoordinatesWhenParking { get; set; }
+        public DateTime DateTimeWhenParking { get; set; } = DateTime.Now;
+        public int MinutesSpentFindingParking { get; set; }
+        public int MinutesParked { get; set; }
+    }
+}
