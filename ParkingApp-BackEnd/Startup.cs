ceacecd04a408d12631fe18ecc229736e13ed2f8
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-06-09 20:30:30 +0200

    Update data seeding and allow cors

--- ParkingApp-BackEnd/Startup.cs
+++ ParkingApp-BackEnd/Startup.cs
@@ -28,8 +28,7 @@ namespace ParkingApp_BackEnd
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-
+        { 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -49,6 +48,13 @@ namespace ParkingApp_BackEnd
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParkingApp_BackEnd v1"));
             }
+
+            app.UseCors(options => options
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
 
             app.UseHttpsRedirection();
 
